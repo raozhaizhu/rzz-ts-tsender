7b8b029d951891a91da15894fb0dadd12a1f3a3e
raozhaizhu <raozhaizhu@gmail.com>
2025-07-14 18:42:41 +0800

    optimize ui and ux

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -9,27 +9,24 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { chainsToTSender, tsenderAbi, erc20Abi } from "@/constants";
-import { useAccount, useChainId, useConfig } from "wagmi";
-import { readContract } from "@wagmi/core";
-import { isAddress } from "viem";
+import { useAccount, useChainId, useConfig, useWriteContract } from "wagmi";
+import { readContract, waitForTransactionReceipt } from "@wagmi/core";
+import { formatEther, isAddress, parseEther } from "viem";
 import { useMemo } from "react";
-import { calculateTotal } from "@/utils/calculateTotal/calculateTotal";
+import { calculateTotal } from "@/utils";
 
 const AirDropFormSchemaClient = z.object({
     tokenAddress: z
         .string()
         .min(1, {
-            message: "tokenAddress is required.",
+            message: "tokenAddress are required.",
         })
         .refine((val) => isAddress(val), { message: "Must be a valid Ethereum address" }),
-    recipients: z
-        .string()
-        .min(1, {
-            message: "recipients is required.",
-        })
-        .refine((val) => isAddress(val), { message: "Must be a valid Ethereum address" }),
+    recipients: z.string().min(1, {
+        message: "recipients are required.",
+    }),
     amounts: z.string().min(1, {
-        message: "amounts is required.",
+        message: "amounts are required.",
     }),
 });
 
@@ -45,11 +42,13 @@ export function AirDropForm() {
     const { control } = form;
     const tokenAddress = useWatch({ control, name: "tokenAddress" });
     const amounts = useWatch({ control, name: "amounts" });
+    const recipients = useWatch({ control, name: "recipients" });
 
     const chainId = useChainId();
     const config = useConfig();
     const account = useAccount();
     const total: number = useMemo(() => calculateTotal(amounts), [amounts]);
+    const { data: hash, isPending, writeContractAsync } = useWriteContract();
 
     const getApprovedAmount = async (tSenderAddress: string | null): Promise<number> => {
         if (!tSenderAddress) {
@@ -81,12 +80,46 @@ export function AirDropForm() {
 
         const approvedAmount = await getApprovedAmount(tSenderAddress);
         console.log("*** approvedAmount:", approvedAmount, "***");
+
+        if (approvedAmount < total) {
+            const approvalHash = await writeContractAsync({
+                abi: erc20Abi,
+                address: tokenAddress as `0x${string}`,
+                functionName: "approve",
+                args: [tSenderAddress, BigInt(total)],
+            });
+            console.log("*** approvalHash:", approvalHash, "***");
+
+            const approvalReceipt = await waitForTransactionReceipt(config, {
+                hash: approvalHash,
+            });
+            console.log("*** approvalReceipt:", approvalReceipt, "***");
+        } else {
+            const res = await writeContractAsync({
+                abi: tsenderAbi,
+                address: tSenderAddress as `0x${string}`,
+                functionName: "airdropERC20",
+                args: [
+                    tokenAddress,
+                    recipients
+                        .split(/[\n,]+/)
+                        .map((addr) => addr.trim())
+                        .filter((addr) => addr !== ""),
+                    amounts
+                        .split(/[\n,]+/)
+                        .map((amt) => amt.trim())
+                        .filter((amt) => amt !== ""),
+                    BigInt(total),
+                ],
+            });
+            console.log("*** res:", res, "***");
+        }
     }
 
     return (
         <Card>
             <CardHeader>
-                <CardTitle>T-Sender</CardTitle>
+                <CardTitle className="text-2xl">T-Sender</CardTitle>
             </CardHeader>
             <CardContent>
                 <Form {...form}>
@@ -133,8 +166,37 @@ export function AirDropForm() {
                                 </FormItem>
                             )}
                         />
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Transaction Details</CardTitle>
+                                <CardContent>
+                                    <div className="flex flex-col gap-2 mt-4">
+                                        <div className="flex justify-between items-center">
+                                            <p className=" text-black/50">Token Name:</p>
+                                            <p className="font-mono">Mock Token</p>
+                                        </div>
+                                        <div className="flex justify-between items-center ">
+                                            <p className=" text-black/50">Amount (wei):</p>
+                                            <p className="font-mono">{total}</p>
+                                        </div>
+                                        <div className="flex justify-between items-center ">
+                                            <p className=" text-black/50">Amount (tokens):</p>
+                                            <p className="font-mono">{Number(formatEther(BigInt(total))).toFixed(2)}</p>
+                                        </div>
+                                    </div>
+                                </CardContent>
+                            </CardHeader>
+                        </Card>
+                        {/* submit-button */}
                         <Button type="submit" className="w-full">
-                            Send Tokens
+                            {isPending ? (
+                                <>
+                                    <span className="loading loading-spinner loading-md"></span>
+                                    <span> Confirming in wallet...</span>
+                                </>
+                            ) : (
+                                "Send Tokens"
+                            )}
                         </Button>
                     </form>
                 </Form>
